43b8a15b84c03a723edbaf2c2b44dbd128cdfe84
a0lim <https://github.com/a0lim/TIL.git>
2022-07-19 18:50:35 +0900

    delete baekjoon to make new baekjoon 2

--- baeckjoon/Bronze/2292. 벌집/벌집.py
+++
@@ -1,9 +0,0 @@
-N = int(input()) ## count of room
-N_th = 1 ## answer
-max = 1 ## max num in condition(1/2~7/8~19/...)
-
-while N > max: 
-    max += 6*N_th ## move to nex condition
-    N_th +=1
-    
-print(N_th)
